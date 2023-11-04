ef3ec6eff5ee413165b43a79aaf4d6c5e274a91c
VickyBE1994 <vickyvip81@gmail.com>
2023-11-04 18:47:18 -0400

    Day 3 task completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//how to compare two JSON have the sameproperties without order
 //QUESTION 1
+
 let obj1={name:"person1",age:5};
 let obj2={age:5,name:"person1"};
 let JSON1= JSON.stringify(obj1);
@@ -11,9 +13,11 @@ if(JSON1===JSON2)
    
 
 else{
-   console("both or not equal")
+   console.log("both or not equal")
 }
-//QUESTION  2
+
+//
+//QUESTION  2//Display all countries flags
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all","true");
 request.send();
@@ -23,16 +27,17 @@ request.onload= function(){
    var result = JSON.parse(data);
    //console.log(result)
    for(let i = 0 ; i< result.length ; i++){
-      console.log(result[i].flags);
+      console.log(result[i].flags.png);
    }   
 
 }
-//QUESTION 3
-var request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all","true");
-request.send();
-request.onload= function(){
-   var data= request.response;
+
+//QUESTION 3 :all countries nam ,region,subregion,population
+var request1 = new XMLHttpRequest();
+request1.open("GET","https://restcountries.com/v3.1/all","true");
+request1.send();
+request1.onload= function(){
+   var data= request1.response;
   // console.log(data);
    var result = JSON.parse(data);
    //console.log(result)
